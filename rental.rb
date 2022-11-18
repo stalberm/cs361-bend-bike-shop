10d6942115bd90bc9e43b63bdc3c35477d605966
Micah Stalberg <stalberm@oregonstate.edu>
2022-11-18 11:30:37 -0800

    Isolated vulnerable external messages in rental

--- rental.rb
+++ rental.rb
@@ -8,11 +8,23 @@ class Rental
   end
 
   def price
-    bike.price + bike.weight*2 + luggage.weight*2
+    bike_price + bike_weight*2 + luggage_weight*2
   end
 
   def weight
-    bike.weight + luggage.weight
+    bike_weight + luggage_weight
+  end
+
+  def bike_price
+    bike.price
+  end
+
+  def bike_weight
+    bike.weight
+  end
+
+  def luggage_weight
+    luggage.weight
   end
 
 end
